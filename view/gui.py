2a08fe4c87770bfe09a066f04e35b1159b994c60
Robin PARISOT <rparisot@nvl.fr>
2025-02-25 13:50:18 +0100

    refacto gui

--- view/gui.py
+++ view/gui.py
@@ -1,4 +1,10 @@
-﻿def create_gui(root, camera):
+﻿# view/gui.py
+import tkinter as tk
+from tkinter import ttk
+from view.styles import apply_style
+from controller.input_handler import update_value, send_data, reset_values, on_entry_change
+
+def create_gui(root, camera):
     # Appliquer le style
     apply_style(root)
 
@@ -68,3 +74,28 @@
     ttk.Button(button_frame, text="Fermer", command=close_application).grid(row=0, column=3, padx=5)
 
     return camera, pitch_entry, yaw_entry, roll_entry, x_entry, y_entry, z_entry
+
+def create_input_row(frame, label_text, camera, attribute, row, min_val, max_val):
+    ttk.Label(frame, text=label_text).grid(row=row, column=2, padx=5, pady=5)
+    entry = ttk.Entry(frame, width=10, justify="center")
+    entry.insert(0, "0")
+    entry.grid(row=row, column=3, padx=5, pady=5)
+#     ttk.Button(frame, text="-10", command=lambda: update_value(entry, -10, camera, attribute)).grid(row=row, column=1, padx=2, pady=5)
+#     ttk.Button(frame, text="-5", command=lambda: update_value(entry, -5, camera, attribute)).grid(row=row, column=0, padx=2, pady=5)
+#     ttk.Button(frame, text="+5", command=lambda: update_value(entry, +5, camera, attribute)).grid(row=row, column=4, padx=2, pady=5)
+#     ttk.Button(frame, text="+10", command=lambda: update_value(entry, +10, camera, attribute)).grid(row=row, column=5, padx=2, pady=5)
+    entry.bind("<KeyRelease>", lambda event: on_entry_change(event, camera, attribute))
+
+    # Ajouter un slider
+    slider = ttk.Scale(frame, from_=min_val, to=max_val, orient="horizontal", command=lambda val: on_slider_change(val, entry, camera, attribute))
+    slider.set(0)
+    slider.grid(row=row, column=1, padx=4, pady=5, sticky="ew")
+
+    return entry, slider
+
+def on_slider_change(val, entry, camera, attribute):
+    value = float(val)
+    entry.delete(0, tk.END)
+    entry.insert(0, str(value))
+    setattr(camera, attribute, value)
+    send_data(camera)

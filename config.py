080b87bf6f501616aec25a9c521cc2e1091aacb4
N445 <robin.global@pm.me>
2025-02-26 22:35:31 +0100

    remove build folder from git

--- config.py
+++
@@ -1,9 +0,0 @@
-﻿# config.py
-CONFIG = {
-    "background_color": "#1e1e1e",  # Couleur de fond sombre
-    "foreground_color": "#ffffff",  # Couleur du texte
-    "button_color": "#ffa500",     # Couleur des boutons (orange)
-    "button_hover_color": "#ff8c00", # Couleur des boutons au survol
-    "entry_background_color": "#333333", # Couleur de fond des champs d'entrée
-    "entry_foreground_color": "#ffffff", # Couleur du texte des champs d'entrée
-}
